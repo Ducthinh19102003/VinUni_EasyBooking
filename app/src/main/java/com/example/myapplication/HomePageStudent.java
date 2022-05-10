6b58ff06b0d6a7d5fda040fe9078d7c68efb3320
Thinh Dao <ducthinh19102003@gmail.com>
2022-05-11 01:32:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/HomePageStudent.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePageStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
