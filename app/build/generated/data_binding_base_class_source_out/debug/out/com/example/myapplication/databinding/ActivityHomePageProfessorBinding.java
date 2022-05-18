350e189c58568330d4a6d4140050818fedd9c182
tuongnguso03 <40336908+tuongnguso03@users.noreply.github.com>
2022-05-18 14:31:38 +0700

    Line separator changes.

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/myapplication/databinding/ActivityHomePageProfessorBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/myapplication/databinding/ActivityHomePageProfessorBinding.java
@@ -4,20 +4,28 @@ package com.example.myapplication.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.myapplication.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityHomePageProfessorBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  private ActivityHomePageProfessorBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final Button button;
+
+  private ActivityHomePageProfessorBinding(@NonNull ConstraintLayout rootView,
+      @NonNull Button button) {
     this.rootView = rootView;
+    this.button = button;
   }
 
   @Override
@@ -43,10 +51,19 @@ public final class ActivityHomePageProfessorBinding implements ViewBinding {
 
   @NonNull
   public static ActivityHomePageProfessorBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.button;
+      Button button = ViewBindings.findChildViewById(rootView, id);
+      if (button == null) {
+        break missingId;
+      }
 
-    return new ActivityHomePageProfessorBinding((ConstraintLayout) rootView);
+      return new ActivityHomePageProfessorBinding((ConstraintLayout) rootView, button);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
