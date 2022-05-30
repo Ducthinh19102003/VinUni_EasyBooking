04fc2de9f50b2f50e9a26dcb2bf6e9bd1ef98201
tuongnguso03 <40336908+tuongnguso03@users.noreply.github.com>
2022-05-30 22:07:03 +0700

    better logout method

--- app/src/main/java/com/example/myapplication/Fragments/AccountFragment.java
+++ app/src/main/java/com/example/myapplication/Fragments/AccountFragment.java
@@ -1,6 +1,9 @@
 package com.example.myapplication.Fragments;
 
+import android.app.AlarmManager;
 import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,6 +25,7 @@ import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.example.myapplication.Login;
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.MyAppGlideModule;
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentAccountBinding;
@@ -251,8 +255,7 @@ public class AccountFragment extends Fragment {
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getActivity(),Login.class));
+                restartApp();
             }
         });
 
@@ -274,6 +277,14 @@ public class AccountFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    private void restartApp() {
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        int mPendingIntentId = 8;
+        PendingIntent mPendingIntent = PendingIntent.getActivity(getContext(), mPendingIntentId, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager mgr = (AlarmManager) getContext().getSystemService(Context.ALARM_SERVICE);
+        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+        System.exit(0);
     }
 
 }
